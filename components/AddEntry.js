2ded263de2d0b0009563949e95cf451bf0c7ef0b
vijitha epa <vepa@bglcorp.com.au>
2019-10-20 20:55:32 +1100

    style slider and reset button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -159,6 +159,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
+        marginTop: 30,
         backgroundColor: white
     },
     row: {
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import {View, Text, Slider} from 'react-native'
+import { Slider, StyleSheet, Text, View } from 'react-native'
+import { gray } from "../utils/colors";
 
 export default function UdaciSlider({max, unit, step, value, onChange}) {
 
     return (
-        <View>
-            <Slider value={value} step={step} maximumValue={max} minimumValue={0} onValueChange={onChange}/>
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
+        <View style={style.row}>
+            <Slider
+                value={value}
+                step={step}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange}
+                style={{flex: 1}}/>
+            <View style={style.metricCounter}>
+                <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
             </View>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center'
+    }
+})
