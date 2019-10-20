6936072630b0308cc8dcf46b0dbd7068ad06ff11
vijitha epa <vepa@bglcorp.com.au>
2019-10-20 19:00:35 +1100

    style Steppers and metrix text of it

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,23 +1,76 @@
 import React from 'react'
-import {View, Text, TouchableOpacity} from 'react-native'
-import {FontAwesome, Entypo} from '@expo/vector-icons'
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Entypo, FontAwesome } from '@expo/vector-icons'
+import { gray, purple, white } from "../utils/colors";
 
 export default function UdacityStepper({max, unit, step, value, onIncrement, onDecrement}) {
 
     return (
-        <View>
-            <View>
-                <TouchableOpacity onPress={onDecrement}>
-                    <FontAwesome name='minus' size={30} color={'black'}/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={onIncrement}>
-                    <FontAwesome name='plus' size={30} color={'black'}/>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
+        <View style={[styles.row, {justifyContent: 'space-between'}]}>
+            {Platform.OS === 'ios'
+                ? <View style={{flexDirection: 'row'}}>
+                    <TouchableOpacity onPress={onDecrement}
+                                      style={[
+                                          styles.iosBtn,
+                                          {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
+                        <FontAwesome name='minus' size={30} color={purple}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={onIncrement}
+                                      style={[
+                                          Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn,
+                                          {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
+                        <FontAwesome name='plus' size={30} color={purple}/>
+                    </TouchableOpacity>
+                </View>
+                : <View style={{flexDirection: 'row'}}>
+                    <TouchableOpacity onPress={onDecrement}
+                                      style={[
+                                          styles.androidBtn,
+                                          {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
+                        <Entypo name='minus' size={30} color={white}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={onIncrement}
+                                      style={[
+                                          Platform.OS === 'ios' ? styles.iosBtn : styles.androidBtn,
+                                          {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
+                        <Entypo name='plus' size={30} color={white}/>
+                    </TouchableOpacity>
+                </View>
+            }
+
+            <View style={styles.metricCounter}>
+                <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    iosBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 5,
+        paddingLeft: 25,
+        paddingRight: 25,
+
+    },
+    androidBtn: {
+        margin: 5,
+        backgroundColor: purple,
+        padding: 10,
+        borderRadius: 2,
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
