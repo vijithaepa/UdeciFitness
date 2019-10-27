9e46b9f31fe472e93b9b23fe731d4e11bf3afe1b
vijitha epa <vepa@bglcorp.com.au>
2019-10-27 17:16:09 +1100

    show MetricCard on Entry Details screen

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,25 +1,40 @@
-import React, {Component} from 'react'
-import {View, Text} from 'react-native'
-import AddEntry from "./AddEntry";
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import {connect} from 'react-redux'
+import MetricCard from "./MetricCard";
+
 
 class EntryDetaail extends Component {
 
-    static navigationOptions = ({navigation})=> {
+    static navigationOptions = ({navigation}) => {
         const {entryId} = navigation.state.params
-        const year = entryId.slice(0,4)
-        const month = entryId.slice(5,7)
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
         const day = entryId.slice(8)
         return {
             title: `${day}/${month}/${year}`
         }
     }
+
     render() {
+        const {metrics, entryId} = this.props
+
         return (
             <View>
+                <MetricCard metrics={metrics}/>
                 <Text>Entry Details - {this.props.navigation.state.params.entryId}</Text>
             </View>
         )
     }
 }
 
-export default EntryDetaail
+
+const mapStateToProps= (state, {navigation}) => {
+    const {entryId} = navigation.state.params
+
+    return {
+        entryId,
+        metrics: state[entryId]
+    }
+}
+export default connect(mapStateToProps)(EntryDetaail)
